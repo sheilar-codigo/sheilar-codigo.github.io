fb4db5539a32cb3ebc9adbb86370e60b44977255
Sheilar <sheilarzune@codigo.sg>
2021-03-30 19:07:55 +0800

    Updates

---
+++ precache-manifest.4e88c96a49bf9c64eaede41b532c44c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965e592b5ed9b72422b8af129eb96965",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "950b8c86f413b0659cbf",
+    "url": "/client/static/css/main.a3487775.chunk.css"
+  },
+  {
+    "revision": "1be03f2a13c2ba441baa",
+    "url": "/client/static/js/2.1d42dd6d.chunk.js"
+  },
+  {
+    "revision": "950b8c86f413b0659cbf",
+    "url": "/client/static/js/main.365fa64c.chunk.js"
+  },
+  {
+    "revision": "5f565e1e41edf81308c8",
+    "url": "/client/static/js/runtime-main.8abd5055.js"
+  }
+]);
